5442037e05acf54abf321cf91110edb4431c0faf
Bryan Summers <bryans@goldcorp.biz>
2017-11-15 16:02:19 +0800

    Refactored product logic out into a ProductModule

--- APM-Start/src/app/app.module.ts
+++ APM-Start/src/app/app.module.ts
@@ -1,45 +1,30 @@
 import { BrowserModule } from '@angular/platform-browser';
-import { FormsModule } from '@angular/forms';
 import { NgModule } from '@angular/core';
 import { HttpClientModule } from '@angular/common/http';
 import { RouterModule } from '@angular/router';
 
 // Components
 import { AppComponent } from './app.component';
-import { ProductListComponent } from './products/product-list.component';
-import { StarComponent } from './shared/star.component';
-import { ProductDetailComponent } from './products/product-detail.component';
 import { WelcomeComponent } from './home/welcome.component';
-
-// Services
-import { ProductGuardService } from './products/product-guard.service'
-
-// Pipes
-import { ConvertToSpacesPipe } from './shared/convert-to-spaces.pipe';
+import { ProductModule } from './products/product.module';
 
 
 @NgModule({
   declarations: [
     AppComponent,
-    ProductListComponent,
-    StarComponent,
-    ConvertToSpacesPipe,
-    ProductDetailComponent,
     WelcomeComponent
   ],
   imports: [
     BrowserModule,
-    FormsModule,
     HttpClientModule,
     RouterModule.forRoot([
-      { path: 'products', component: ProductListComponent },
-      { path: 'products/:id', canActivate: [ProductGuardService], component: ProductDetailComponent },
       { path: 'welcome', component: WelcomeComponent },
       { path: '', redirectTo: 'welcome', pathMatch: 'full'}, // default route on loading the app
       { path: '**', redirectTo: 'welcome', pathMatch: 'full'} // if url path doesn't match any route definitions, redirect to the welcome page
-    ])
+    ]),
+    ProductModule
   ],
-  providers: [ProductGuardService],
+  providers: [],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
---
+++ APM-Start/src/app/products/product.module.ts
@@ -0,0 +1,34 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { ConvertToSpacesPipe } from '../shared/convert-to-spaces.pipe';
+import { ProductDetailComponent } from './product-detail.component';
+import { ProductListComponent } from './product-list.component';
+import { ProductService } from './product.service';
+import { ProductGuardService } from './product-guard.service';
+import { FormsModule } from '@angular/forms';
+import { RouterModule } from '@angular/router';
+import { StarComponent } from '../shared/star.component';
+
+@NgModule({
+  imports: [
+    CommonModule,
+    FormsModule,
+    RouterModule.forChild([
+      { path: 'products', component: ProductListComponent },
+      { path: 'products/:id', canActivate: [ProductGuardService], component: ProductDetailComponent }     
+    ]) // now that we are adding the router module to this 'feature' module, we use forChild(), 
+       // not forRoot(). This is so we don't re-register the RouterService, which forRoot() does automatically
+
+  ],
+  declarations: [
+    ProductListComponent,
+    ProductDetailComponent,
+    StarComponent,
+    ConvertToSpacesPipe
+  ],
+  providers: [
+    ProductService,
+    ProductGuardService
+  ]
+})
+export class ProductModule { }
