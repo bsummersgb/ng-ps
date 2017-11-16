04f0fc55031ff45d6637c18a088e723071de885d
Bryan Summers <bryans@goldcorp.biz>
2017-11-16 12:13:25 +0800

    more refactoring into modules

--- APM-Start/src/app/products/product.module.ts
+++ APM-Start/src/app/products/product.module.ts
@@ -1,19 +1,13 @@
 import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
-import { ConvertToSpacesPipe } from '../shared/convert-to-spaces.pipe';
 import { ProductDetailComponent } from './product-detail.component';
 import { ProductListComponent } from './product-list.component';
 import { ProductService } from './product.service';
 import { ProductGuardService } from './product-guard.service';
-import { FormsModule } from '@angular/forms';
 import { RouterModule } from '@angular/router';
-import { StarComponent } from '../shared/star.component';
 import { SharedModule } from './../shared/shared.module';
 
 @NgModule({
   imports: [
-    CommonModule,
-    FormsModule,
     RouterModule.forChild([// now that we are adding the router module to this 'feature' module, we use forChild(), 
     // not forRoot(). This is so we don't re-register the RouterService, which forRoot() does automatically
       { path: 'products', component: ProductListComponent },
@@ -25,8 +19,6 @@ import { SharedModule } from './../shared/shared.module';
   declarations: [
     ProductListComponent,
     ProductDetailComponent,
-    StarComponent,
-    ConvertToSpacesPipe
   ],
   providers: [
     ProductService,
